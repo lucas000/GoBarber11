d17a0d1fd18c17cfaa9f7140557f094ce719f426
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2020-04-18 12:50:08 -0300

    Implemented routes to create a Appointment and create a User

--- backend/src/models/Appointment.ts
+++ backend/src/models/Appointment.ts
@@ -1,4 +1,13 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn } from 'typeorm';
+
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -6,6 +15,10 @@ class Appointment {
   id: string;
 
   @Column()
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
   provider: string;
 
   @Column('timestamp with time zone')
