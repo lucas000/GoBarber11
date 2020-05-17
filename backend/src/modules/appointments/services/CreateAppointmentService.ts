44b3b0209d9a79374e4ae4ae8a97f389ec2e1ef3
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2020-05-17 13:08:23 -0300

    Routes and controllers

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour, isBefore } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -26,6 +26,13 @@ class CreateAppointmentService {
       throw new AppError("You can't create a appointment on a past date.");
     };
 
+    if (user_id === provider_id) {
+      throw new AppError("You can't create a appointment with yourself.");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError("You can only create appointments between 8am and 5pm.");
+    }
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
